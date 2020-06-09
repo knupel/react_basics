a4804734ce304f0385b87eb2c82a8195e8d44de3
Stan le Punk <stanislasiii@gmail.com>
2020-06-09 11:32:07 +0200

    work on local refresh method for class, class Clock part.

--- src/3_clock.js
+++ src/3_clock.js
@@ -1,23 +1,49 @@
 import React, { Component } from "react";
-import Basic from "./0_basic";
-// import ReactDOM from "react-dom";
+// import Basic from "./0_basic";
+import ReactDOM from "react-dom";
 
+// without constructor, the result is not refresh
+/**
 class Clock extends Component {
   render() {
     return (
       <div className="Clock">
-        <h1>chez vous il est {this.props.date.toLocateTimeString()}</h1>
+        <h1>Chez vous il est {this.props.date.toLocaleTimeString("fr")}</h1>
+      </div>
+    );
+  }
+}
+*/
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  // rect method
+  componentDidMount() {
+    this.timer_id = setInterval(() => this.draw(), 1000);
+  }
+
+  // rect method
+  componentWillUnmount() {
+    clearInterval(this.timer_id);
+  }
+
+  draw() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <h1>Chez vous il est {this.state.date.toLocaleTimeString("fr")}</h1>
       </div>
     );
   }
 }
 
 export default Clock;
-// function tictac() {
-//   ReactDOM.render(
-//     <Clock date={new Date()} />,
-//     document.getElementById("shook-root")
-//   );
-// }
-
-// setInterval(tictac, 1000);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Basic from "./0_basic.js";
@@ -7,20 +7,27 @@ import Welcome from "./1_welcome.js";
 import Multi from "./2_multi.js";
 import Clock from "./3_clock.js";
 
-// import * from *.js;
-
-// import elem_welcome from "./1_Multi.js";
-
-// const elem_welcome = 2;
 export const elem_welcome = <Welcome name="Félix" />;
+
 ReactDOM.render(
   <React.StrictMode>
-    {/* {elem_welcome}
-    <Multi /> */}
+    {elem_welcome}
+    <Multi />
     <Basic name="Stan" nickname="le Punk" />
-    <Clock date={new Date()} />
+    {/* <Clock date={new Date()} /> // this one is for class without constructor*/}
+    <Clock />
   </React.StrictMode>,
 
   // <Multi />,
   document.getElementById("shook-root")
 );
+
+// refresh your page, but it's very raw and not optimize for all
+// function draw() {
+//   ReactDOM.render(
+//     <Clock date={new Date()} />,
+//     document.getElementById("shook-root")
+//   );
+// }
+
+// setInterval(draw, 1000);
