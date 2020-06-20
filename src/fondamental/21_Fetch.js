a46d34e97abe855ecd18ea986a54e9d67d4be26f
Stan le Punk <stanislasiii@gmail.com>
2020-06-20 11:33:30 +0200

    updata data file

--- src/fondamental/21_Fetch.js
+++ src/fondamental/21_Fetch.js
@@ -1,38 +1,46 @@
 import React from 'react';
+/*
+ * That's don't work because I don't understand to attempts data on server.
+ */
 
 export class Fetch extends React.Component {
   constructor() {
     super();
     this.state = {
+      loading: false,
       character: {
-        // name: 'Luke Skywalker',
-        // height: '172',
-        // mass: '77',
-        // hair_color: 'blond',
-        // skin_color: 'fair',
-        // eye_color: 'blue',
-        // birth_year: '19BBY',
-        // gender: 'male'
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        hair_color: 'blond',
+        skin_color: 'fair',
+        eye_color: 'blue',
+        birth_year: '19BBY',
+        gender: 'male'
       }
     };
   }
 
   componentDidMount() {
     console.log('componentDidMount()');
-    fetch('https://swapi.co/api/people/1')
+    this.setState({ loading: true });
+    // the problem is here... I don't know how make a API with data
+    // when I put data on github there is a problem CORS to attemps
+    // fetch('../src/data')
       .then((responde) => responde.json())
       .then((data) => {
         this.setState({
+          loading: false,
           charactere: data
         });
       });
   }
 
   render() {
-    // const info = this.state.character.name;
+    const info = this.state.loading ? 'loading...' : this.state.character.name;
     return (
       <div>
-        <p>{this.state.character.name}</p>
+        <p>{info}</p>
       </div>
     );
   }
